54104e8581b523dde0bb114c5acf8be16a86f90e
Debora2019 <dmutiz@gmail.com>
2021-05-07 15:45:59 +0100

    project started

---
+++ employeemanage/src/main/java/tech/project/employeemanage/EmployeemanageApplication.java
@@ -0,0 +1,13 @@
+package tech.project.employeemanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeemanageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeemanageApplication.class, args);
+	}
+
+}
